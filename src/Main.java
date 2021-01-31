bcfb8cbfc9dac474cb56173132bfddfb85bcf627
ktonyi <antonigorevich.k@gmail.com>
2021-01-31 21:46:09 +0300

    add SRC

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public  static  void  main ( String [ ]  args ) {
+        int ticketprice = 105;
+        int bonusMiles = ticketprice/20;
+        System.out.println(bonusMiles);
+    }
+}
